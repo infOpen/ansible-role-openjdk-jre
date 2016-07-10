d794651e6bc2fabee6f4b5e420cb17f1f38f7aa3
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-07-10 23:36:18 +0200

    Update pytest plugin and add xenial testing

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -3,15 +3,28 @@ Role tests
 """
 import pytest
 
-pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh')
+pytestmark = pytest.mark.destructive
 
 
-def test_packages(Package):
+@pytest.mark.docker_images('infopen/ubuntu-trusty-ssh:0.1.0')
+def test_trusty_packages(Package):
     """
-    Tests about packages installed
+    Tests about packages installed on Trusty
     """
 
     packages = ['python-apt-common', 'python-apt', 'openjdk-7-jre']
+
+    for package in packages:
+        assert Package(package).is_installed is True
+
+
+@pytest.mark.docker_images('infopen/ubuntu-xenial-ssh-py27:0.1.0')
+def test_xenial_packages(Package):
+    """
+    Tests about packages installed on Xenial
+    """
+
+    packages = ['python-apt-common', 'python-apt', 'openjdk-9-jre']
 
     for package in packages:
         assert Package(package).is_installed is True
